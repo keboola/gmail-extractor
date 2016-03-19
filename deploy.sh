d1a34012f1de2ad280d9a94ca21a3f2efc52e9e5
Vladimír Kriška <vlado@keboola.com>
2016-03-20 00:48:59 +0100

    Output of docker images instead of docker ps -a

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 docker build -t keboola/gmail-extractor .
 docker login -e="." -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker tag keboola/gmail-extractor quay.io/keboola/gmail-extractor:$TRAVIS_TAG
-docker ps -a
+docker images
 docker push quay.io/keboola/gmail-extractor:$TRAVIS_TAG
