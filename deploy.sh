fd22b3226fed414b82c04b6e879fd98d34f70aea
Vladimír Kriška <vlado@keboola.com>
2016-03-19 22:44:10 +0100

    Show git untracked and ignored files before build

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git status -uall --ignored
 docker build -t keboola/gmail-extractor .
 docker login -e="." -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker tag keboola/gmail-extractor quay.io/keboola/gmail-extractor:$TRAVIS_TAG
