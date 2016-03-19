1f6b437dad06085463f0d5d909e3fae2260d6961
Vladimír Kriška <vlado@keboola.com>
2016-03-19 23:24:40 +0100

    Revert "Show git untracked and ignored files before build"
    This reverts commit fd22b3226fed414b82c04b6e879fd98d34f70aea.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-git status -uall --ignored
 docker build -t keboola/gmail-extractor .
 docker login -e="." -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker tag keboola/gmail-extractor quay.io/keboola/gmail-extractor:$TRAVIS_TAG
