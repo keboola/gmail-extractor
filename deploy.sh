262a40f9791ba1f1dbada1cc4c5ccacce8a437c3
Vladimír Kriška <vlado@keboola.com>
2016-03-19 17:37:09 +0100

    Simple script to push built image to quay

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker build -t keboola/gmail-extractor .
+docker login -e="." -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
+docker tag keboola/gmail-extractor quay.io/keboola/gmail-extractor:$TRAVIS_TAG
+docker ps -a
+docker push quay.io/keboola/gmail-extractor:$TRAVIS_TAG
