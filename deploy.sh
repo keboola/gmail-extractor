5717588896ecd376b6190936a1cdd39729c1e3bb
Vladimír Kriška <vlado@keboola.com>
2018-04-05 21:36:40 +0200

    Tune deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
-DP_VENDOR_ID="keboola"
-DP_APP_ID="keboola.ex-gmail"
+set -e
 
-docker pull quay.io/keboola/developer-portal-cli-v2:latest \
-&& export REPOSITORY=`docker run --rm \
-  -e KBC_DEVELOPERPORTAL_USERNAME=$DP_USERNAME \
-  -e KBC_DEVELOPERPORTAL_PASSWORD=$DP_PASSWORD \
-  -e KBC_DEVELOPERPORTAL_URL=$DP_URL \
-  quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository $DP_VENDOR_ID $DP_APP_ID` \
-&& docker tag keboola/gmail-extractor $REPOSITORY:$TRAVIS_TAG \
-&& docker tag keboola/gmail-extractor $REPOSITORY:latest \
-&& docker images \
-&& eval $(docker run --rm \
-  -e KBC_DEVELOPERPORTAL_USERNAME=$DP_USERNAME \
-  -e KBC_DEVELOPERPORTAL_PASSWORD=$DP_PASSWORD \
-  -e KBC_DEVELOPERPORTAL_URL=$DP_URL \
-  quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login $DP_VENDOR_ID $DP_APP_ID) \
-&& docker push $REPOSITORY:$TRAVIS_TAG \
-&& docker push $REPOSITORY:latest \
-&& docker run --rm \
-  -e KBC_DEVELOPERPORTAL_USERNAME=$DP_USERNAME \
-  -e KBC_DEVELOPERPORTAL_PASSWORD=$DP_PASSWORD \
-  -e KBC_DEVELOPERPORTAL_URL=$DP_URL \
-  quay.io/keboola/developer-portal-cli-v2:latest update-app-repository $DP_VENDOR_ID $DP_APP_ID $TRAVIS_TAG
+KBC_DEVELOPERPORTAL_VENDOR="keboola"
+KBC_DEVELOPERPORTAL_APP="keboola.ex-gmail"
+APP_IMAGE="keboola/gmail-extractor"
+
+# Obtain the component repository and log in
+docker pull quay.io/keboola/developer-portal-cli-v2:latest
+export REPOSITORY=`docker run --rm  \
+    -e KBC_DEVELOPERPORTAL_USERNAME \
+    -e KBC_DEVELOPERPORTAL_PASSWORD \
+    quay.io/keboola/developer-portal-cli-v2:latest \
+    ecr:get-repository ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP}`
+eval $(docker run --rm \
+    -e KBC_DEVELOPERPORTAL_USERNAME \
+    -e KBC_DEVELOPERPORTAL_PASSWORD \
+    quay.io/keboola/developer-portal-cli-v2:latest \
+    ecr:get-login ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP})
+
+# Push to the repository
+docker tag ${APP_IMAGE}:latest ${REPOSITORY}:${TRAVIS_TAG}
+docker tag ${APP_IMAGE}:latest ${REPOSITORY}:latest
+docker push ${REPOSITORY}:${TRAVIS_TAG}
+docker push ${REPOSITORY}:latest
+
+# Update the tag in Keboola Developer Portal -> Deploy to KBC
+if echo ${TRAVIS_TAG} | grep -c '^v\?[0-9]\+\.[0-9]\+\.[0-9]\+$'
+then
+    docker run --rm \
+        -e KBC_DEVELOPERPORTAL_USERNAME \
+        -e KBC_DEVELOPERPORTAL_PASSWORD \
+        quay.io/keboola/developer-portal-cli-v2:latest \
+        update-app-repository ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP} ${TRAVIS_TAG} ecr ${REPOSITORY}
+else
+    echo "Skipping deployment to KBC, tag ${TRAVIS_TAG} is not allowed."
+fi
